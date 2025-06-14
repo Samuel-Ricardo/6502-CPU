0995ae9691e88618f6933fbd0c081bab278675e9
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-14 15:59:09 -0300

    [ :sparkles: ] | create: cpu - execute > instruction (cpu::operation)

--- src/public/cpu/6502.h
+++ src/public/cpu/6502.h
@@ -42,4 +42,45 @@ struct CPU : virtual public Flags,
     Cycles--;
     return Data;
   }
+
+  void Execute(uint32_t Cycles, Memory &memory) {
+    while (Cycles > 0) {
+      Byte Instruction = FetchByte(Cycles, memory);
+
+      switch (Instruction) {
+
+      case INS_LDA_IM: {
+        Byte Value = FetchByte(Cycles, memory);
+        A = Value;
+        LDASetStatus();
+      } break;
+
+      case INS_LDA_ZP: {
+        Byte ZeroPageAddress = FetchByte(Cycles, memory);
+        A = ReadByte(Cycles, ZeroPageAddress, memory);
+        LDASetStatus();
+      } break;
+
+      case INS_LDA_ZPX: {
+        Byte ZeroPageAddress = FetchByte(Cycles, memory);
+        ZeroPageAddress += X;
+        Cycles--;
+        A = ReadByte(Cycles, ZeroPageAddress, memory);
+        LDASetStatus();
+      } break;
+
+      case INS_JSR: {
+        Word SubroutineAddress = FetchWord(Cycles, memory);
+        SP -= 2;
+        memory.WriteWord(PC - 1, SP, Cycles);
+        PC = SubroutineAddress;
+        Cycles--;
+      } break;
+
+      default: {
+        printf("Instruction not implemented or handled: 0x%X\n", Instruction);
+      } break;
+      }
+    }
+  }
 };
